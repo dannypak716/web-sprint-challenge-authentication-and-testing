f8f10e1101b93fbddadf2922cfa806ce57c04848
Jeong Pak <dannypak716@gmail.com>
2021-12-17 19:06:33 -0500

    boilerplate for testing. don't think i can complete project in time

--- api/server.test.js
+++ api/server.test.js
@@ -1,3 +1,7 @@
+const request = require('supertest');
+const server = require('./server');
+const db = require('../data/dbConfig')
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
